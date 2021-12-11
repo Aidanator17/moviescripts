2a393b3cd01dadc73c09134b3b4d2de4958d9dd7
Aidan Christopher <gandamovies@gmail.com>
2021-12-10 22:34:27 -0800

    its working! needs work still

--- scriptsearch.js
+++ scriptsearch.js
@@ -1,19 +1,64 @@
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
 const SerpApi = require('google-search-results-nodejs');
 const search = new SerpApi.GoogleSearch("175ce01d5c1a9ceefca08e78e77eba4c6b43611c975b1cb0628b36d34afac19c");
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const port = process.env.PORT || 8000;
 
-let movie = "Joker"
+const app = express();
 
-const params = {
-  q: "imsdb "+movie,
-  location: "Austin, Texas, United States",
-  hl: "en",
-  gl: "us",
-  google_domain: "google.com"
-};
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
+// Middleware for express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-const callback = function(data) {
-  console.log(data["organic_results"][0]['link']);
-};
+app.listen(port, async () => {
+  console.log(`🚀 Server has started on port ${port}`);
+});
 
-// Show result as JSON
-search.json(params, callback);
+app.get("/", async (req, res) => {
+  res.render('index')
+})
+
+app.post("/", async (req, res) => {
+
+  let movie = req.body.movie
+
+  const params = {
+    q: "imsdb " + movie,
+    location: "Austin, Texas, United States",
+    hl: "en",
+    gl: "us",
+    google_domain: "google.com"
+  };
+
+  const callback = async function (data) {
+    let link = data["organic_results"][0]['link'];
+    const response = await fetch(link)
+    const text = await response
+    const $ = cheerio.load(await text.text());
+    // console.log(await text.text())
+    res.render("results", {script:$("#mainbody > table:nth-child(3) > tbody > tr > td:nth-child(3) > table > tbody > tr > td > pre").html()})
+  };
+
+  // Show result as JSON
+  search.json(params, callback);
+
+})
